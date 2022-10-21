bdffc831eb9464721ae16c4317e97612d40aa53c
pepehaggeb <99206621+PedroHaggeBaptista@users.noreply.github.com>
2022-10-21 01:21:50 -0300

    admin call page creation

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,22 +1,27 @@
 import Link from 'next/link';
+import axios from '../axios';
 import { useEffect, useState } from 'react';
 import Header from '@components/Header';
 import Layout from '@components/Layout';
 import Github from '@assets/Github';
 import Linkedin from '@assets/Linkedin';
 
+import { GetServerSideProps } from 'next';
+import { toast } from 'react-toastify';
+import { useRouter } from 'next/router';
+
 // require('dotenv').config()
 
 const Admin = () => {
   const [data, setData] = useState([]);
 
-  const loadSubscriptions = async () => {};
+  const loadSubscriptions = async () => { };
 
-  useEffect(() => {}, []);
+  useEffect(() => { }, []);
 
-  const onReload = async () => {};
+  const onReload = async () => { };
 
-  const deleteSub = async (id: string) => {};
+  const deleteSub = async (id: string) => { };
 
   return (
     <Layout>
@@ -127,6 +132,31 @@ const Admin = () => {
       </div>
     </Layout>
   );
+};
+
+// This gets called on every request
+export const getServerSideProps: GetServerSideProps = async ctx => {
+  let cookieToken = ctx.req.cookies['token'];
+
+  console.log(cookieToken)
+
+  const redirect = (errorMessage: string) => {
+    ctx.res.writeHead(301, {
+      Location: '/?tokenError=true&errorMessage=' + errorMessage
+    });
+    ctx.res.end();
+    // Pass data to the page via props
+    return;
+  };
+
+  // try {
+  //   await axios.get(`/Subscription/admin/${cookieToken}`);
+  // } catch (err) {
+  //   redirect(err.response.data);
+  // }
+
+  // Pass data to the page via props
+  return { props: {} };
 };
 
 export default Admin;
