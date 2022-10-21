52a29b337df7e59f168834fc655c05489f4ce996
marcelofeitoza <feitoza.marcelo7@gmail.com>
2022-10-21 01:27:30 -0300

    creating dev branch

--- axios.ts
+++ axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const instance: any
     = axios.create({
-        baseURL: process.env.NEXT_PUBLIC_SERVER_URL
+        baseURL: 'http://localhost:3001',
     })
 
 console.log(instance)
