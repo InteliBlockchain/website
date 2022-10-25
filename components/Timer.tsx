5633825e4b4d8bcf5b4e2e90911b7a270fb874ae
marcelofeitoza <feitoza.marcelo7@gmail.com>
2022-10-25 18:07:50 -0300

    fixed timer

--- components/Timer.tsx
+++ components/Timer.tsx
@@ -1,53 +1,58 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 
 const Timer = () => {
-    const [dateDiff, setDateDiff] = useState<{
-        days: number | string
-        hours: number | string
-        minutes: number | string
-        seconds: number | string
-    }>({
-        days: '00',
-        hours: '00',
-        minutes: '00',
-        seconds: '00',
-    })
+  const [dateDiff, setDateDiff] = useState<{
+    days: number | string;
+    hours: number | string;
+    minutes: number | string;
+    seconds: number | string;
+  }>({
+    days: '00',
+    hours: '00',
+    minutes: '00',
+    seconds: '00'
+  });
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            const dateNow = new Date()
-            const dateFuture = new Date(2022, 9, 26, 0, 0, 0)
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const dateNow = new Date();
+      const dateFuture = new Date(2022, 9, 26, 0, 0, 0);
 
-            const diff = dateFuture.getTime() - dateNow.getTime()
+      const diff = dateFuture.getTime() - dateNow.getTime();
 
-            const days = Math.floor(diff / (1000 * 60 * 60 * 24))
-            const hours = Math.floor((diff / (1000 * 60 * 60)) % 24)
-            const minutes = Math.floor((diff / 1000 / 60) % 60)
-            const seconds = Math.floor((diff / 1000) % 60)
+      const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+      const minutes = Math.floor((diff / 1000 / 60) % 60);
+      const seconds = Math.floor((diff / 1000) % 60);
 
-            setDateDiff({
-                days,
-                hours,
-                minutes,
-                seconds,
-            })
-        }, 1000)
+      setDateDiff({
+        days,
+        hours,
+        minutes,
+        seconds
+      });
+    }, 1000);
 
-        return () => {
-            clearInterval(interval)
-        }
-    }, [dateDiff])
+    return () => {
+      clearInterval(interval);
+    };
+  }, [dateDiff]);
 
-    if (dateDiff.days !== 0) {
-        return (
-            <span>
-                Faltam {dateDiff.days} dias, {dateDiff.hours} horas, {dateDiff.minutes} minutos e {dateDiff.seconds}{' '}
-                segundos...
-            </span>
-        )
-    }
+  if (
+    dateDiff.days === 0 &&
+    dateDiff.hours === 0 &&
+    dateDiff.minutes === 0 &&
+    dateDiff.seconds === 0
+  ) {
+    return <p>Inscrições encerradas</p>;
+  }
 
-    return null
-}
+  return (
+    <span>
+      Faltam {dateDiff.days} dias, {dateDiff.hours} horas, {dateDiff.minutes}{' '}
+      minutos e {dateDiff.seconds} segundos...
+    </span>
+  );
+};
 
-export default Timer
+export default Timer;
