b5fad5d94681f986c06009cc18e36c9184809dd0
marcelofeitoza <feitoza.marcelo7@gmail.com>
2022-12-01 02:00:13 -0300

    changing favicon

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,26 +1,14 @@
-import React, { useEffect } from "react";
-import Footer from "./Footer";
-import Head from "next/head";
-import { LayoutProps } from "@typescript/types";
-import { ToastContainer } from "react-toastify";
+import React from 'react';
+import Footer from './Footer';
+import Head from 'next/head';
+import { LayoutProps } from '@typescript/types';
+import { ToastContainer } from 'react-toastify';
 
-const Layout = ({ 
-  children, 
-  title,
+const Layout = ({
+  children,
+  title = 'This is the default title',
   hide = false
 }: LayoutProps) => {
-  useEffect(() => {
-    if (
-      localStorage.theme === "dark" ||
-      (!("theme" in localStorage) &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches)
-    ) {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
-  }, []);
-
   return (
     <div className="flex-1 w-full h-full md:pb-8 select-none">
       <Head>
